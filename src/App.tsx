73bb496b357b1795635a266fff8caff0f2e5c0bb
SEBAX49 <solorzano5365@gmail.com>
2026-03-06 20:08:35 -0400

    guardar

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <h1>Proyecto</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+/*const cartaEjemplo = { numero: '1', elemento: 'fuego'};
+function Carta({carta}) {
+    return <h1>Hola soy Carta {carta.numero}{carta.fuego} !</h1>;
+} 
+export default function app() {
+    return (
+        <div>
+            <Carta carta={cartaEjemplo}/>
+        </div>
+    );
+}*/
+
+
+
+const saludarAlumno = (nombre: string) => {
+    return 'hola, ${nombre}, bienvenido a la clase de React con typerScript';
 }
-
-export default App
-                                                              
